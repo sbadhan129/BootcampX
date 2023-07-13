21572d489378e804b7617e75d7a5e5e057a720b8
Sourav <souravbadhan909@gmail.com>
2023-07-13 19:25:21 -0400

    Connecting to the bootcampx database in teachers.js & add code for PostgreSQL query to find all the teachers that made an assistance request during a certain cohort with parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const cohortName = process.argv[2];
+
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name =$1
+ORDER BY teacher;`
+
+const values = [cohortName];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+})
+.catch(err => console.error('query error', err.stack));
